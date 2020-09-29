447dcad012d863efc79db512fa2ab6511a0e76af
vinmugambi <vinmugambi17@gmail.com>
2020-09-29 12:55:01 +0300

    add blog meta

--- content/articles/first.md
+++ content/articles/first.md
@@ -1,3 +1,11 @@
-# My first blog post
+---
+title: My First Blog Post
+description: Learning how to use @nuxt/content to create a blog
+img: first.jpg
+alt: my first blog post
+---
 
-Welcome to my first blog post using content module
+
+# My first blog post
+Making a blog using nuxt content module
+
--- pages/blog/_slug.vue
+++ pages/blog/_slug.vue
@@ -1,21 +1,40 @@
 <template>
   <article>
+    <h1>{{ article.title }}</h1>
+    <p>{{ article.description }}</p>
+    <img :src="article.img" :alt="article.alt" />
+    <p>Article last updated: {{ formatDate(article.updatedAt) }}</p>
     <nuxt-content :document="article" />
-<p>Post last updated: {{formatDate(article.updatedAt) }}</p>  </article>
+  </article>
 </template>
 
 <script>
-  export default {
-    async asyncData({ $content, params }) {
-      const article = await $content('articles', params.slug).fetch()
+export default {
+  async asyncData({ $content, params }) {
+    const article = await $content("articles", params.slug).fetch();
 
-      return { article }
-    },
-    methods: {
+    return { article };
+  },
+  methods: {
     formatDate(date) {
-      const options = { year: 'numeric', month: 'short', day: 'numeric' }
-      return new Date(date).toLocaleDateString('en-GB', options)
-    }
- }
-  }
+      const options = { year: "numeric", month: "short", day: "numeric" };
+      return new Date(date).toLocaleDateString("en-GB", options);
+    },
+  },
+};
 </script>
+
+<style>
+  .nuxt-content h2 {
+    font-weight: bold;
+    font-size: 28px;
+  }
+  .nuxt-content h3 {
+    font-weight: bold;
+    font-size: 22px;
+  }
+  .nuxt-content p {
+    margin-bottom: 20px;
+  }
+</style>
+
